d51e668b34614652290917da36e1cc27e1994e60
MingzhiYe16 <1626384862@qq.com>
2023-03-08 20:55:32 -0800

    user can set base number

--- main.py
+++ main.py
@@ -34,10 +34,13 @@ def generateSamplePool(PoolSize):
 
     return
 
-def getSample(PoolSize,SampleSize,errorProb=0.01):
+def getSample(PoolSize,SampleSize,BaseNumber,errorProb=0.01):
     global ProbOfEachClonotype, realDF
     # ProbOfEachClonotype=[1 for i in range(PoolSize)]
-    ProbOfEachClonotype = [random.uniform(0.6,0.9) ** i for i in range(PoolSize)]
+    if BaseNumber>0:
+        ProbOfEachClonotype = [random.uniform(0.6,0.9) ** i for i in range(PoolSize)]
+    else:
+        ProbOfEachClonotype = [BaseNumber ** i for i in range(PoolSize)]
     ProbOfEachClonotype = [x if x > 0.1 ** 7 else 0.1 ** 7 for x in ProbOfEachClonotype]
     ProbSum = sum(ProbOfEachClonotype)
     ProbOfEachClonotype = [i / ProbSum for i in ProbOfEachClonotype]
